71d65d45bc9cf340779cf360f240c0b3fc2cc128
zmj27404 <zmj27404@sina.cn>
2016-10-22 17:30:21 +0800

    Initial Commit

---
+++ SixClub2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SixClub2
+//
+//  Created by zmj27404 on 22/10/2016.
+//  Copyright © 2016 zmj27404. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
